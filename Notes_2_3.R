a3eb9b1f1e0a23ba1ed9f66e34be85ee13854006
jacksondial <jacksondial11@gmail.com>
2021-09-08 14:55:43 -0400

    More notes

--- Notes_2_3.R
+++ Notes_2_3.R
@@ -10,8 +10,25 @@ ggplot(iris, aes(x = Species, y = Sepal.Width))+
 ggplot(iris, aes(x = Species, y = Sepal.Width))+
   geom_violin()
 
+set.seed(11)
+#plot(mpg ~ cyl, mtcars)
 
 
+#SBP among american adults without pre-diabetes or diabetes is N(118, 225). Note that
+#SBP >= 140 mmHg is indicative of hypertension and 130 <= SBP < 140 is indicative of
+#pre-hypertension
+
+#1
+pnorm(130, 118,15)
+
+#2
+(pnorm(140, 118,15)) - pnorm(130, 118, 15)
+
+#3
+1- pnorm(140, 118, 15)
+
+#4
+qnorm(.1, 118, 15)
 
 
 
