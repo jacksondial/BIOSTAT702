ed2de977374945cfaac5ffbf9309afaa3d8da57a
jacksondial <jacksondial11@gmail.com>
2021-10-20 09:46:54 -0400

    Notes 7 Continued

--- Notes_7.R
+++ Notes_7.R
@@ -49,4 +49,38 @@ qf(.01,30,25)
 pf(.1668, 10,4)
 
 
+# We collect a sample of 𝑛1 = 222 females and determine that 78 of them have prediabetes. 
+# We collect a sample of 𝑛2 = 121 males and determine that 47 have prediabetes. 
+# A 99% CI for the difference in proportion of females and males who have
+# pre-diabetes is given below.
+
+n1 <- 222
+p1 <- 78/222
+n2 <- 121
+p2 <- 47/121
+zcrit <- qnorm(1-0.01/2)
+p1-p2 + c(-zcrit, zcrit) * sqrt(p1*(1-p1) / n1 + p2*(1-p2)/n2)
+
+
+
+zcrit <- qnorm(1-.1/2)
+ddiff <- 5
+sig1 <- sqrt(35)
+sig2 <- sqrt(38)
+n1 <- n2 <- 40
+
+bbeta <- pnorm(zcrit - ddiff / sqrt(sig1^2/n1 + sig2^2 / n2)) -
+  pnorm(-zcrit - ddiff / sqrt(sig1^2/n1 + sig2^2 / n2))
+#this is the type II error probability^^^
+
+power <- 1- bbeta
+power
+# When doing power/sample size calculations we must have:
+# Desired difference
+# type I error
+# sample sizes
+# estimated variance
+# hypotheses
+
+#Check slide 7-47 & 7-48 for R functions for two sample tests
 
